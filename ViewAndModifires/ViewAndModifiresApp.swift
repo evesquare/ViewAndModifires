cc33792afc01e0752a5829693f5d5b501fbb4590
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-18 20:03:09 +0900

    Initial Commit

---
+++ ViewAndModifires/ViewAndModifiresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiresApp.swift
+//  ViewAndModifires
+//
+//  Created by evesquare on 2025/04/18.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewAndModifiresTests/ViewAndModifiresTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiresTests.swift
+//  ViewAndModifiresTests
+//
+//  Created by evesquare on 2025/04/18.
+//
+
+import Testing
+@testable import ViewAndModifires
+
+struct ViewAndModifiresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
