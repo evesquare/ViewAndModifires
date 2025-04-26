28aeffa78c5908fa50140d34d6fdc56b98a471e5
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-26 17:24:02 +0900

    Add Title and Watermark view modifiers for enhanced styling

--- ViewAndModifires/ContentView.swift
+++ ViewAndModifires/ContentView.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 16))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+        
+    }
+}
+
+extension View {
+    func watermarked(text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(text: "Hacking")
         }
         .padding()
     }
